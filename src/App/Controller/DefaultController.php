38e62a3e1c71b4cd1664fad3e22846581c3bd09a
kuzmaarek1 <95350850+kuzmaarek1@users.noreply.github.com>
2025-07-29 22:01:54 +0200

    Add modal

--- src/App/Controller/DefaultController.php
+++ src/App/Controller/DefaultController.php
@@ -8,6 +8,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+// Dodany enum z obsługiwanymi walutami
+enum SupportedCurrency: string
+{
+    case EUR = 'EUR';
+    case USD = 'USD';
+    case CZK = 'CZK';
+    case IDR = 'IDR';
+    case BRL = 'BRL';
+
+    public function label(): string
+    {
+        return match($this) {
+            self::EUR => 'euro',
+            self::USD => 'dolar amerykański',
+            self::CZK => 'korona czeska',
+            self::IDR => 'rupia indonezyjska',
+            self::BRL => 'real (Brazylia)',
+        };
+    }
+}
+
 
 class DefaultController extends AbstractController
 {
---
+++ src/App/Entity/ExchangeRate.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use App\Controller\SupportedCurrency;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'exchange_rate')]
+class ExchangeRate
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    public ?int $id = null;
+
+    #[ORM\Column(type: 'date')]
+    public \DateTimeInterface $date;
+
+    #[ORM\Column(type: 'string', length: 3, enumType: SupportedCurrency::class)]
+    public SupportedCurrency $currencyCode;
+
+    #[ORM\Column(type: 'float')]
+    public float $mid;
+} 
